073df338216fdff1f5f846b244ad553eaf5e9b93
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2021-06-24 14:09:29 +0300

    Add proposition-as-types diagrams to natural model

--- src/natural_model.lean
+++ src/natural_model.lean
@@ -2,7 +2,6 @@
 -- by Steve Awodey on the Polynomial Functor Workshop
 -- https://youtu.be/RDuNIP4icKI?list=PLhgq-BqyZ7i7R-fGcAmNyWmJBQg1wzex-&t=10765
 
-import data.iso
 import functors.family
 
 -- Type' is a pointed type
@@ -23,11 +22,65 @@ def pie (c : fam Type*) : Type* := Π i, c i
 def sig (c : fam Type*) : Type* := Σ i, c i
 
 def lam (c : fam Type') : Type' := ⟨(pie ∘ functor.map p) c, λ i, (c i).2⟩
+
 -- https://youtu.be/RDuNIP4icKI?t=12445
 def p_pie_pullback : p ∘ lam = pie ∘ functor.map p := rfl
 
 def Q := sig $ fam.of sig
 def q (x : Q) : fam Type* := x.1
 def pair (x : Q) : Type' := ⟨(sig ∘ q) x, x.2⟩
+
 -- https://youtu.be/RDuNIP4icKI?t=12866
 def p_sig_pullback : p ∘ pair = sig ∘ q := rfl
+
+def s (X : Type*) : Prop := nonempty X
+def i (X : Prop) : Type* := inhabited X
+
+-- https://youtu.be/RDuNIP4icKI?t=13639
+def trunc : Type* → Type* := i ∘ s
+def s_i_eq_id : s ∘ i = id :=
+begin
+  funext p,
+  apply propext,
+  cases classical.em p with x x,
+  { have y : nonempty (inhabited p) := ⟨⟨x⟩⟩,
+    exact iff.intro (const x) (const y) },
+  cases classical.em (nonempty (inhabited p)) with y y,
+  { induction y,
+    induction y,
+    contradiction },
+  { apply iff.intro _ _,
+    repeat { contradiction } }
+end
+
+def all (c : fam Prop) : Prop := ∀ i, c i
+def exi (c : fam Prop) : Prop := ∃ i, c i
+
+-- https://youtu.be/RDuNIP4icKI?t=13681
+def all_as_pie : all = s ∘ pie ∘ fam.map i :=
+begin
+  funext c,
+  apply propext _,
+  induction c with I c,
+  apply iff.intro (λ x, _) (λ x, _),
+  { exact nonempty.intro (λ y, inhabited.mk (x y)) },
+  { induction x,
+    intro y,
+    have z : inhabited (c y) := x y,
+    induction z,
+    exact z }
+end
+
+-- https://youtu.be/RDuNIP4icKI?t=13681
+def exi_as_sig : exi = s ∘ sig ∘ fam.map i :=
+begin
+  funext c,
+  apply propext _,
+  induction c with I c,
+  apply iff.intro (λ x, _) (λ x, _),
+  { exact nonempty.intro ⟨classical.some x, inhabited.mk (classical.some_spec x)⟩ },
+  { induction x,
+    induction x with y z,
+    induction z,
+    exact ⟨y, z⟩ }
+end
